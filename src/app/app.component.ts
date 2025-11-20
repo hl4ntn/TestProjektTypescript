f3f8057ca74e1cbcd3737d358e716a007434e285
fl4ntn <flynn.anton@gmx.de>
2025-11-20 13:44:28 +0100

    start project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { FruitlistComponent } from "./fruitlist/fruitlist.component";
+import { HeaderComponent } from './shared/components/header/header.component';
+import { RouterOutlet } from "../../node_modules/@angular/router";
+import { FooterComponent } from './shared/components/footer/footer.component';
+
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, FruitlistComponent, HeaderComponent, RouterOutlet, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'TestProjektTypescript';
+  
+
 }
